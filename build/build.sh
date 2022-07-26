36722b0799288d30f23367b0dea3208c4b912038
Butch Warns <mail@butchwarns.com>
2022-07-26 16:20:48 +0200

    Clean build scripts and CMake setup

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#! /bin/bash
 
 # Allow this script to be run from different working directory without messing up relative paths
 scriptdir="$(dirname "$0")"
 cd "$scriptdir"
 
-cmake -S ./source -B ./build
+cmake . -GUnix\ Makefiles -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug
+
+cmake --build . --parallel #--config Debug
