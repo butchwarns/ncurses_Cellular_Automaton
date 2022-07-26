848589c8bab8ea2a66a80a1a1acc3e843d5f50a3
Butch Warns <mail@butchwarns.com>
2022-07-26 19:26:05 +0200

    Fix small bug in entering rule manually

--- source/Display.cpp
+++ source/Display.cpp
@@ -24,246 +24,236 @@ SOFTWARE.
 
 #include "Display.hpp"
 
-Display::Display(int _automatonWinRows, int _automatonWinCols) :
-    automatonWinRows (_automatonWinRows), automatonWinCols (_automatonWinCols),
-    position (1)
+Display::Display(int _automatonWinRows, int _automatonWinCols) : automatonWinRows(_automatonWinRows), automatonWinCols(_automatonWinCols),
+                                                                 position(1)
+{
+
+    // Create new window
+    automatonWin = createWindow(automatonWinRows, automatonWinCols, 0, 0);
+
+    // Set canvas size to automatonWin size without border
+    int canvasCellNum = (_automatonWinRows - 2) * (_automatonWinCols - 2);
+
+    // Create canvases for scrolling animation
+    canvas = new bool[canvasCellNum];
+    nextCanvas = new bool[canvasCellNum];
+
+    // Initialise canvases as all false
+    for (int cell = 0; cell < canvasCellNum; cell++)
     {
-
-        // Create new window
-        automatonWin = createWindow (automatonWinRows, automatonWinCols, 0, 0);
-
-        // Set canvas size to automatonWin size without border
-        int canvasCellNum = (_automatonWinRows - 2) * (_automatonWinCols - 2);
-
-        // Create canvases for scrolling animation
-        canvas = new bool [canvasCellNum];
-        nextCanvas = new bool [canvasCellNum];
-
-        // Initialise canvases as all false
-        for (int cell = 0; cell < canvasCellNum; cell++)
-            {
-                canvas [cell] = false;
-                nextCanvas [cell] = false;
-            }
+        canvas[cell] = false;
+        nextCanvas[cell] = false;
     }
-
+}
 
 Display::~Display()
-    {
-        delwin (automatonWin);
-    }
+{
+    delwin(automatonWin);
+}
 
+WINDOW *Display::createWindow(int rows, int cols, int winY, int winX)
+{
+    // Create window
+    WINDOW *window = newwin(rows, cols, winY, winX);
+    // Inform ncurses about new window
+    refresh();
 
-WINDOW* Display::createWindow (int rows, int cols, int winY, int winX)
-    {
-        // Create window
-        WINDOW *window = newwin (rows, cols, winY, winX);
-        // Inform ncurses about new window
-        refresh();
+    // Draw box around window
+    box(window, 0, 0);
 
-        // Draw box around window
-        box (window, 0, 0);
+    // Make box visible
+    wrefresh(window);
 
-        // Make box visible
-        wrefresh (window);
-
-        // Return pointer to created window
-        return window;
-    }
-
+    // Return pointer to created window
+    return window;
+}
 
 void Display::reset()
-    {
-        //Clear screen
-        werase (automatonWin);
+{
+    // Clear screen
+    werase(automatonWin);
 
-        // Redraw box
-        box (automatonWin, 0, 0);
+    // Redraw box
+    box(automatonWin, 0, 0);
 
-        // Make changes visible
-        wrefresh (automatonWin);
+    // Make changes visible
+    wrefresh(automatonWin);
 
-        // Set position back to first line
-        position = 1;
-    }
+    // Set position back to first line
+    position = 1;
+}
 
-
-void Display::displayGUI (int rule, bool* ruleBits, bool seedRandomized)
-    {
-       displayRule (rule, ruleBits);
-       displaySeedRandomized (seedRandomized);
-    }
-
+void Display::displayGUI(int rule, bool *ruleBits, bool seedRandomized)
+{
+    displayRule(rule, ruleBits);
+    displaySeedRandomized(seedRandomized);
+}
 
 void Display::advancePosition()
-    {
-        position += 1;
-    }
+{
+    position += 1;
+}
 
-
-void Display::setPosition (int _position)
-    {
-        position = _position;
-    }
-
+void Display::setPosition(int _position)
+{
+    position = _position;
+}
 
 int Display::getPosition()
-    {
-        return position;
-    }
-
+{
+    return position;
+}
 
 int Display::getAutomatonWinRows()
+{
+    return automatonWinRows;
+}
+
+void Display::displayState(bool *state, int pos)
+{
+    // Iterate over cells and print the living ones
+    for (int cell = 0; cell < (automatonWinCols - 2); cell++)
     {
-        return automatonWinRows;
+        if (state[cell])
+            mvwaddch(automatonWin, pos, cell + 1, ACS_CKBOARD);
+        else
+            mvwaddch(automatonWin, pos, cell + 1, ' ');
     }
 
+    wrefresh(automatonWin);
+}
 
-void Display::displayState (bool *state, int pos)
+void Display::displayRule(int rule, bool *ruleBits)
+{
+    // Pad rule number with leading zeros
+    std::stringstream strstr;
+    strstr << std::setw(3) << std::setfill('0') << rule;
+
+    // Print rule in top left corner
+    std::string ruleStr = "Rule: " + strstr.str();
+    mvwaddstr(automatonWin, 0, 0, ruleStr.c_str());
+    wrefresh(automatonWin);
+
+    // Print rule bits also in top left corner
+    std::string ruleBitStr = " | ";
+    for (int bit = 7; bit >= 0; bit--)
+    {
+        if (ruleBits[bit])
+            ruleBitStr.push_back('1');
+        else
+            ruleBitStr.push_back('0');
+    }
+    ruleBitStr.push_back(' ');
+    waddstr(automatonWin, ruleBitStr.c_str());
+    wrefresh(automatonWin);
+}
+
+void Display::displaySeedRandomized(bool seedRandomized)
+{
+    // Create string
+    std::string yesNo = seedRandomized ? "YES" : "NO";
+    std::string seedRandomizedStr = "";
+
+    if (seedRandomized)
+    {
+        seedRandomizedStr = " Seed Randomized: " + yesNo;
+    }
+    else
+    {
+        seedRandomizedStr = " Seed Randomized: " + yesNo;
+    }
+
+    // Print in top right corner
+    int topRight = automatonWinCols - seedRandomizedStr.length();
+
+    mvwaddstr(automatonWin, 0, topRight, seedRandomizedStr.c_str());
+    wrefresh(automatonWin);
+}
+
+void Display::advanceCanvas(bool *state)
+{
+    int canvasRows = automatonWinRows - 2;
+    int canvasCols = automatonWinCols - 2;
+
+    // Shift all lines up by one dropping the oldest one
+    for (int row = 0; row < (canvasRows - 1); row++)
+    {
+        for (int cell = 0; cell < canvasCols; cell++)
+        {
+            nextCanvas[(row * canvasCols) + cell] = canvas[((row + 1) * canvasCols) + cell];
+        }
+    }
+
+    // Add new state in last line of next canvas
+    for (int cell = 0; cell < canvasCols; cell++)
+    {
+        nextCanvas[((canvasRows - 1) * canvasCols) + cell] = state[cell];
+    }
+
+    // Pointer swap to reuse memory
+    bool *temp = canvas;
+    canvas = nextCanvas;
+    nextCanvas = temp;
+}
+
+void Display::displayCanvas()
+{
+    // For each row
+    for (int row = 0; row < automatonWinRows - 2; row++)
     {
         // Iterate over cells and print the living ones
         for (int cell = 0; cell < (automatonWinCols - 2); cell++)
-            {
-                if (state [cell])
-                    mvwaddch (automatonWin, pos, cell + 1, ACS_CKBOARD);
-                else
-                    mvwaddch (automatonWin, pos, cell + 1, ' ');
-            }
-
-        wrefresh (automatonWin);
+        {
+            if (canvas[row * (automatonWinCols - 2) + cell])
+                mvwaddch(automatonWin, row + 1, cell + 1, ACS_CKBOARD);
+            else
+                mvwaddch(automatonWin, row + 1, cell + 1, ' ');
+        }
     }
-
-
-void Display::displayRule (int rule, bool* ruleBits)
-    {
-        // Pad rule number with leading zeros
-        std::stringstream strstr;
-        strstr << std::setw(3) << std::setfill('0') << rule;
-
-        // Print rule in top left corner
-        std::string ruleStr = "Rule: " + strstr.str();
-        mvwaddstr (automatonWin, 0, 0, ruleStr.c_str());
-        wrefresh (automatonWin);
-
-        // Print rule bits also in top left corner
-        std::string ruleBitStr =" | ";
-        for (int bit = 7; bit >= 0; bit--)
-            {
-                if (ruleBits [bit])
-                    ruleBitStr.push_back ('1');
-                else
-                    ruleBitStr.push_back ('0');
-            }
-        ruleBitStr.push_back (' ');
-        waddstr (automatonWin, ruleBitStr.c_str());
-        wrefresh (automatonWin);
-    }
-
-
-
-void Display::displaySeedRandomized (bool seedRandomized)
-    {
-        // Create string
-        std::string yesNo = seedRandomized ? "YES" : "NO";
-        std::string seedRandomizedStr = "";
-
-        if (seedRandomized)
-            {
-                seedRandomizedStr = " Seed Randomized: " + yesNo;
-            }
-        else
-            {
-                seedRandomizedStr = " Seed Randomized: " + yesNo;
-            }
-
-        // Print in top right corner
-        int topRight = automatonWinCols - seedRandomizedStr.length();
-
-        mvwaddstr (automatonWin, 0, topRight, seedRandomizedStr.c_str());
-        wrefresh (automatonWin);
-    }
-
-
-void Display::advanceCanvas (bool *state)
-    {
-        int canvasRows = automatonWinRows - 2;
-        int canvasCols = automatonWinCols - 2;
-
-        // Shift all lines up by one dropping the oldest one
-        for (int row = 0; row < (canvasRows - 1); row++)
-            {
-                for (int cell = 0; cell < canvasCols; cell++)
-                    {
-                        nextCanvas [(row * canvasCols) + cell] = canvas [((row + 1) * canvasCols) + cell];
-                    }
-            }
-
-        // Add new state in last line of next canvas
-        for (int cell = 0; cell < canvasCols; cell++)
-            {
-                nextCanvas [((canvasRows - 1) * canvasCols) + cell] = state [cell];
-            }
-
-        // Pointer swap to reuse memory
-        bool* temp = canvas;
-        canvas = nextCanvas;
-        nextCanvas = temp;
-    }
-
-
-void Display::displayCanvas()
-    {
-        // For each row
-        for (int row = 0; row < automatonWinRows - 2; row++)
-            {
-                // Iterate over cells and print the living ones
-                for (int cell = 0; cell < (automatonWinCols - 2); cell++)
-                    {
-                        if (canvas [row * (automatonWinCols - 2) + cell])
-                            mvwaddch (automatonWin, row + 1, cell + 1, ACS_CKBOARD);
-                        else
-                            mvwaddch (automatonWin, row + 1, cell + 1, ' ');
-                    }
-            }
-        wrefresh (automatonWin);
-    }
-
+    wrefresh(automatonWin);
+}
 
 int Display::enterRule()
+{
+    std::string enterStr = "Enter Rule:         ";
+
+    // Print message in top left corner
+    mvwaddstr(automatonWin, 0, 0, enterStr.c_str());
+
+    // Show the cursor and echo key presses
+    curs_set(1);
+    echo();
+    wrefresh(automatonWin);
+
+    // Have user enter the rule
+    wmove(automatonWin, 0, 12);
+
+    char numberStr[4] = "123";
+    wgetnstr(automatonWin, numberStr, 3);
+    if (numberStr == nullptr)
     {
-        std::string enterStr = "Enter Rule:         ";
-
-        // Print message in top left corner
-        mvwaddstr (automatonWin, 0, 0, enterStr.c_str());
-
-        // Show the cursor and echo key presses
-        curs_set (1);
-        echo();
-        wrefresh (automatonWin);
-
-        // Have user enter the rule
-        wmove(automatonWin, 0, 12);
-
-        char numberStr [3];
-        wgetnstr (automatonWin, numberStr, 3);
-
-        // Convert to int
-        std::stringstream strstr (numberStr);
-
-        int rule = 0;
-        strstr >> rule;
-
-        // Hide cursor again and don't echo key presses
-        curs_set (0);
-        noecho();
-        wrefresh (automatonWin);
-
-        if (rule >= 0 && rule <= 255)
-            {
-                return rule;
-            }
-        else
-            {
-                return 0;
-            }
+        mvwaddstr(automatonWin, 0, 0, "INVALID RULE!        ");
+        wrefresh(automatonWin);
+        return 0;
     }
+
+    // Convert to int
+    std::stringstream strstr(numberStr);
+
+    int rule = 0;
+    strstr >> rule;
+
+    // Hide cursor again and don't echo key presses
+    curs_set(0);
+    noecho();
+    wrefresh(automatonWin);
+
+    if (rule >= 0 && rule <= 255)
+    {
+        return rule;
+    }
+    else
+    {
+        return 0;
+    }
+}
